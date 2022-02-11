c2d27419795e182bcdb9f495a825dc0f99e1fecd
Kamil Adam Jabłoński <kamilcrafter12@gmail.com>
2022-02-11 17:02:48 +0100

    Added some more handling for database

--- Core/DatabaseHandling.cs
+++ Core/DatabaseHandling.cs
@@ -4,6 +4,7 @@ using System.Data.SQLite;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ZeiControl.Core
@@ -35,5 +36,112 @@ namespace ZeiControl.Core
                 $"INSERT INTO temp (sensorType, value) VALUES ('{type}', {value});";
             command.ExecuteNonQuery();
         } 
+
+        public static void SaveToNewTable(SQLiteConnection connection, string tablename)
+        {
+            if (Regex.IsMatch(tablename, @"^\d+") || tablename.Length < 4)
+            {
+                SaveSensorDataWindow.DefaultTableLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.TableExistsLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.ErrorLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.SuccessLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.IllegalCharLabel.Visibility = System.Windows.Visibility.Visible;
+                SaveSensorDataWindow.TableNotExists.Visibility = System.Windows.Visibility.Hidden;
+                return;
+            }
+
+            SQLiteCommand command;
+
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText =
+                    $"CREATE TABLE \"{tablename}\" (\"id\" INTEGER NOT NULL UNIQUE, \"sensorType\" TEXT NOT NULL, \"value\" INTEGER NOT NULL, \"sqlTimestamp\" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(\"id\" AUTOINCREMENT));";
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+                SaveSensorDataWindow.DefaultTableLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.TableExistsLabel.Visibility = System.Windows.Visibility.Visible;
+                SaveSensorDataWindow.ErrorLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.SuccessLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.IllegalCharLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.TableNotExists.Visibility = System.Windows.Visibility.Hidden;
+                return;
+            }
+
+            SQLiteDataReader sqlite_datareader;
+            command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM temp;";
+
+            sqlite_datareader = command.ExecuteReader();
+
+            while(sqlite_datareader.Read())
+            {
+                string type = sqlite_datareader.GetString(1);
+                int value = sqlite_datareader.GetInt32(2);
+                DateTime dateTime = sqlite_datareader.GetDateTime(3);
+
+                command = connection.CreateCommand();
+
+                command.CommandText =
+                    $"INSERT INTO \"{tablename}\" (sensorType, value, sqlTimestamp) VALUES ('{type}', {value}, '{dateTime}');";
+                command.ExecuteNonQuery();
+            }
+
+            SaveSensorDataWindow.DefaultTableLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.TableExistsLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.ErrorLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.SuccessLabel.Visibility = System.Windows.Visibility.Visible;
+            SaveSensorDataWindow.IllegalCharLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.TableNotExists.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        public static void AppendToExistingTable(SQLiteConnection connection, string tablename)
+        {
+            SQLiteCommand command;
+            SQLiteDataReader sqlite_datareader;
+
+            command = connection.CreateCommand();
+            command.CommandText = $"SELECT * FROM sqlite_sequence WHERE name = '{tablename}';";
+            sqlite_datareader = command.ExecuteReader();
+
+            if(!sqlite_datareader.HasRows)
+            {
+                SaveSensorDataWindow.DefaultTableLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.TableExistsLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.ErrorLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.SuccessLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.IllegalCharLabel.Visibility = System.Windows.Visibility.Hidden;
+                SaveSensorDataWindow.TableNotExists.Visibility = System.Windows.Visibility.Visible;
+                return;
+            }
+
+            command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM temp;";
+
+            sqlite_datareader = command.ExecuteReader();
+
+            while (sqlite_datareader.Read())
+            {
+                string type = sqlite_datareader.GetString(1);
+                int value = sqlite_datareader.GetInt32(2);
+                DateTime dateTime = sqlite_datareader.GetDateTime(3);
+
+                command = connection.CreateCommand();
+
+                command.CommandText =
+                    $"INSERT INTO \"{tablename}\" (sensorType, value, sqlTimestamp) VALUES ('{type}', {value}, '{dateTime}');";
+                command.ExecuteNonQuery();
+            }
+
+            SaveSensorDataWindow.DefaultTableLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.TableExistsLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.ErrorLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.SuccessLabel.Visibility = System.Windows.Visibility.Visible;
+            SaveSensorDataWindow.IllegalCharLabel.Visibility = System.Windows.Visibility.Hidden;
+            SaveSensorDataWindow.TableNotExists.Visibility = System.Windows.Visibility.Hidden;
+        }
     }
 }
--- SaveSensorDataWindow.xaml.cs
+++ SaveSensorDataWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +19,25 @@ namespace ZeiControl
 
     public partial class SaveSensorDataWindow : Window
     {
+        public static TextBox InputTableNameTextBox { get; set; }
+        public static Label DefaultTableLabel { get; set; }
+        public static Label TableExistsLabel { get; set; }
+        public static Label ErrorLabel { get; set; }
+        public static Label SuccessLabel { get; set; }
+        public static Label IllegalCharLabel { get; set; }
+        public static Label TableNotExists { get; set; }
+
         public SaveSensorDataWindow()
         {
             InitializeComponent();
+
+            InputTableNameTextBox = TableNameBox;
+            DefaultTableLabel = DefaultNameLabel;
+            TableExistsLabel = TableNameExistsLabel;
+            ErrorLabel = ErrorNameLabel;
+            SuccessLabel = TableNameSuccessLabel;
+            IllegalCharLabel = IllegalCharacterLabel;
+            TableNotExists = TableNotExistsLabel;
         }
 
         //Move window override
@@ -32,6 +50,58 @@ namespace ZeiControl
         private void CloseSaveDataWindow_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void SaveTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            string tableName = InputTableNameTextBox.Text;
+
+            SQLiteConnection connection;
+            connection = Core.DatabaseHandling.CreateConnection();
+
+            try
+            {
+                Core.DatabaseHandling.SaveToNewTable(connection, tableName);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Exception encountered: ");
+                Trace.WriteLine(ex.Message);
+
+                DefaultTableLabel.Visibility = Visibility.Hidden;
+                TableExistsLabel.Visibility = Visibility.Hidden;
+                ErrorLabel.Visibility = Visibility.Visible;
+                SuccessLabel.Visibility = Visibility.Hidden;
+                IllegalCharLabel.Visibility = Visibility.Hidden;
+                TableNotExists.Visibility = Visibility.Hidden;
+            }
+            connection.Close();
+        }
+
+        private void AppendTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            string tableName = InputTableNameTextBox.Text;
+
+            SQLiteConnection connection;
+            connection = Core.DatabaseHandling.CreateConnection();
+
+            try
+            {
+                Core.DatabaseHandling.AppendToExistingTable(connection, tableName);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Exception encountered: ");
+                Trace.WriteLine(ex.Message);
+
+                DefaultTableLabel.Visibility = Visibility.Hidden;
+                TableExistsLabel.Visibility = Visibility.Hidden;
+                ErrorLabel.Visibility = Visibility.Visible;
+                SuccessLabel.Visibility = Visibility.Hidden;
+                IllegalCharLabel.Visibility = Visibility.Hidden;
+                TableNotExists.Visibility = Visibility.Hidden;
+            }
+            connection.Close();
         }
     }
 }
